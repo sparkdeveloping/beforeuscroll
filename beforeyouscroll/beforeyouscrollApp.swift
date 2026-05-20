322b9be44675931df62dbdcf5d89e4b499a9ed8b
Denzel Nyatsanza <sbj.developing@gmail.com>
2026-05-20 15:25:16 -0500

    Initial Commit

---
+++ beforeyouscroll/beforeyouscrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  beforeyouscrollApp.swift
+//  beforeyouscroll
+//
+//  Created by Denzel Nyatsanza on 5/20/26.
+//
+
+import SwiftUI
+
+@main
+struct beforeyouscrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
